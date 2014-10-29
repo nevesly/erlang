c097788a8a7e9331790587dd24a7fe676347b69c
U-HYB70-20121218H\Administrator <Administrator@seven.(none)>
2014-10-29 17:07:56 +0800

    delete no use files

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec erl -pa ebin edit deps/*/ebin -boot start_sasl -config myrebarapp -setcookie myrebarapp -sname myrebarapp1 -s inets -s lager -s reloader -s myrebarapp
+exec erl -pa ebin edit deps/*/ebin -boot start_sasl -config myapp -setcookie myrebarapp -sname myrebarapp1 -s inets -s lager -s reloader -s myrebarapp
