070665135c735287336527afafa497167f8bc7eb
nevesly <nevesly1109@gmail.com>
2014-10-30 18:42:49 +0800

    tcp connection agent

---
+++ src/net/conn_agent.erl
@@ -0,0 +1,8 @@
+%%%-----------------------------------------------------------------------------
+%%% @author Seven Lu, <nevesly1109@gmail.com>
+%%%  [http://www.sevenlu.com/]
+%%% @copyright (C) 2014. Seven Lu
+%%% @doc tcp connection agent
+%%% @end
+%%%-----------------------------------------------------------------------------
+-module(conn_agent).
