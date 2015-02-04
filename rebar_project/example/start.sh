155edbcac9ad01edbe263f8bd70af1b8b12ae622
nevesly <nevesly1109@gmail.com>
2015-02-04 12:08:07 +0800

    fix bug

--- rebar_project/example/start.sh
+++ rebar_project/example/start.sh
@@ -3,8 +3,8 @@ exec erl -pa ebin edit deps/*/ebin \
     -boot start_sasl \
     -config myapp \
     -setcookie myrebarapp \
-    -sname myrebarapp1 \
+    -name seven_rebar_app@127.0.0.1 \
     -s inets \
     -s lager \
     -s reloader \
-    -s myapp . .
+    -s myapp start
