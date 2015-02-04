d6d204f46d6aebf789def634af58cbad11fc803b
nevesly <nevesly1109@gmail.com>
2015-02-04 11:43:34 +0800

    easy to read

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-exec erl -pa ebin edit deps/*/ebin -boot start_sasl -config myapp -setcookie myrebarapp -sname myrebarapp1 -s inets -s lager -s reloader -s myapp
+exec erl -pa ebin edit deps/*/ebin \
+    -boot start_sasl \
+    -config myapp \
+    -setcookie myrebarapp \
+    -sname myrebarapp1 \
+    -s inets \
+    -s lager \
+    -s reloader \
+    -s myapp . .
